72143d508508a5917a89c410a91229be8454f464
Toema04 <s.toema04@gmail.com>
2024-01-08 14:15:58 +0200

    fixing stuff three

--- pages/about/index.js
+++ pages/about/index.js
@@ -195,7 +195,7 @@ const About = () => {
           className="flex flex-col w-full xl:max-w-[48%] xl:h-[400px] xl:mt-3">
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 md:mb-2 xl:mb-4">
             {aboutData.map((item, itemIndex) => {
-              return (
+              return (<>
                 <div
                   key={itemIndex}
                   className=
@@ -206,12 +206,12 @@ const About = () => {
                 >
                   {item.title}
                 </div>
-              );
+                </>);
             })}
           </div>
           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
             {aboutData[index].info.map((item, itemIndex) => {
-              return (
+              return (<>
                 <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60 cursor-default">
                   {/* title */}
                   <div className="font-light mb-2 md:mb-0 cursor-default">{item.title}</div>
@@ -220,15 +220,15 @@ const About = () => {
                   <div className="flex gap-x-4">
                     {/* icons */}
                     {item.icons?.map((icon, itemIcon) => {
-                      return (
+                      return (<>
                         <div key={itemIcon} className="text-2xl text-white">
                           {icon}
-                        </div>
+                        </div></>
                       )
                     })}
                   </div>
                 </div>
-              )
+                </>)
             })}
           </div>
         </motion.div>
