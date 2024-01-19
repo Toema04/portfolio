c8dae70989f3a2c8f5fdeae340f51e6e1f0bfaf1
Toema04 <s.toema04@gmail.com>
2024-01-20 01:12:07 +0200

    updating portfolio link

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -2,6 +2,7 @@ import { Particles } from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import React, { useCallback } from "react";
 
+
 const ParticlesContainer = () => {
   
   const particlesInit = useCallback(async (engine) => {
